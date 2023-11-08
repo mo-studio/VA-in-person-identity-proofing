53059490a4ba82526b1066bd74e51303e8879dfc
“Anu <anu@themostudio.com>
2023-11-08 13:30:53 -0800

    lint changes

--- app/src/components/intake/LocationOptions.jsx
+++ app/src/components/intake/LocationOptions.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Radio } from "@trussworks/react-uswds";
 
 import { IntakeContext } from "../../contexts/IntakeContext";
--- app/src/components/proofing/SelectIDType.js
+++ app/src/components/proofing/SelectIDType.js
@@ -26,14 +26,16 @@ export default function SelectIDType() {
       >
         <option>- Select - </option>
         <option value="Lousiana Driver's License">
-          Lousiana Driver's License
+          Lousiana {"Driver's"} License
         </option>
-        <option value="Maine Driver's License">Maine Driver's License</option>
+        <option value="Maine Driver's License">
+          Maine {"Driver's"} License
+        </option>
         <option value="Maryland Driver's License">
-          Maryland Driver's License
+          Maryland {"Driver's"} License
         </option>
         <option value="Massachusetts Driver's License">
-          Massachusetts Driver's License
+          Massachusetts {"Driver's"} License
         </option>
       </Select>
     </p>
