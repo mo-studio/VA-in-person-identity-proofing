dff2f77c5a6d2381d1dbb61cc9495cbec9054e74
“Anu <anu@themostudio.com>
2023-11-09 14:20:32 -0800

    new tsx files

---
+++ app/src/components/proofing/SelectOptions.tsx
@@ -0,0 +1,9 @@
+export const driversLicenseOptions = (
+  <>
+    <option>- Select - </option>
+    <option value="LA">Lousiana {"Driver's"} License</option>
+    <option value="ME">Maine {"Driver's"} License</option>
+    <option value="MD">Maryland {"Driver's"} License</option>
+    <option value="MA">Massachusetts {"Driver's"} License</option>
+  </>
+);
