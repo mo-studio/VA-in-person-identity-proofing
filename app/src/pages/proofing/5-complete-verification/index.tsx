07a5ecb7fdb9b607a0fa2aa0a53f930e88752d9a
Mateo Salinas Clarke <mateosalinasclarke@proton.me>
2023-11-16 00:06:22 -0700

    add Summary Box content and mock data

--- app/src/pages/proofing/5-complete-verification/index.tsx
+++ app/src/pages/proofing/5-complete-verification/index.tsx
@@ -1,19 +1,59 @@
 import Link from "next/link";
-import { ChangeEvent, useContext } from "react";
-import { StepIndicator, StepIndicatorStep } from "@trussworks/react-uswds";
+// import { ChangeEvent, useContext } from "react";
+import {
+  StepIndicator,
+  StepIndicatorStep,
+  SummaryBox,
+  SummaryBoxContent,
+  SummaryBoxHeading,
+} from "@trussworks/react-uswds";
 
 export default function CaseNumerPage() {
+  // TODO: Replace hardcoded data with context from previous pages
+  const tempFirstName = "Isabel";
+  const tempMiddleInitial = "D.";
+  const tempLastName = "Parsons";
+  const tempSSN = "123-45-6789";
+  const tempDOB = "12/03/1954";
+  const tempAddress1 = "231 Briarwood Road";
+  const tempAddress2 = "";
+  const tempCity = "Baltimore";
+  const tempState = "Maryland";
+  const tempZip = "21222";
+  // end TODO
+
   return (
     <div className="page">
       <div className="container">
         <StepIndicator counters="small" headingLevel="h4">
-          <StepIndicatorStep label="Confirm email" />
-          <StepIndicatorStep label="Validate ID" />
-          <StepIndicatorStep label="Fill in information" />
+          <StepIndicatorStep label="Confirm email" status="complete" />
+          <StepIndicatorStep label="Validate ID" status="complete" />
+          <StepIndicatorStep label="Fill in information" status="complete" />
           <StepIndicatorStep label="Complete verification" status="current" />
         </StepIndicator>
 
         <h1>Complete verification</h1>
+
+        <SummaryBox>
+          <SummaryBoxHeading headingLevel="h3">
+            Applicant Summary
+          </SummaryBoxHeading>
+          <SummaryBoxContent>
+            Name: {tempFirstName} {tempMiddleInitial} {tempLastName}
+            <br />
+            Social Security Number: {tempSSN}
+            <br />
+            DOB: {tempDOB}
+            <br />
+            Address: {tempAddress1}
+            {tempAddress2}, {tempCity}, {tempState} {tempZip}
+          </SummaryBoxContent>
+        </SummaryBox>
+
+        <p>
+          To adjust the information in this summary box, click ‘Back’ and adjust
+          the content in the form.
+        </p>
 
         <div className="text">
           <div className="wrapper button-wrapper">
