1819944e47f708bea0739021f111d10f832bc09c
Mateo Salinas Clarke <mateosalinasclarke@proton.me>
2023-11-20 17:21:34 -0700

    Add content for’/6-verification-task-closed’

--- app/src/pages/proofing/6-verification-task-closed/index.tsx
+++ app/src/pages/proofing/6-verification-task-closed/index.tsx
@@ -1,17 +1,36 @@
+import { useContext } from "react";
 import { StepIndicator, StepIndicatorStep } from "@trussworks/react-uswds";
 
+import { ProofingContext } from "../../../contexts/ProofingContext";
+
 export default function CaseNumerPage() {
+  const contextValue = useContext(ProofingContext);
+  const { proofingData } = contextValue || {
+    proofingData: { idType: "", isDocumentValidated: false, caseNumber: "" },
+  };
   return (
     <div className="page">
       <div className="container">
+        <h3>
+          <b>Case Number: {proofingData.caseNumber}</b>
+        </h3>
         <StepIndicator counters="small" headingLevel="h4">
-          <StepIndicatorStep label="Confirm email" status="current" />
-          <StepIndicatorStep label="Validate ID" />
-          <StepIndicatorStep label="Fill in information" />
-          <StepIndicatorStep label="Complete verification" />
+          <StepIndicatorStep label="Confirm email" status="complete" />
+          <StepIndicatorStep label="Validate ID" status="complete" />
+          <StepIndicatorStep label="Fill in information" status="complete" />
+          <StepIndicatorStep label="Complete verification" status="complete" />
         </StepIndicator>
 
-        <h1>Verification task closed</h1>
+        <h1 className="padding-x-0">Verification task closed</h1>
+        <p className="text-bold">
+          You have completed all of the steps of In-Person Identity Proofing for
+          Case Number {proofingData.caseNumber}.
+        </p>
+        <p>
+          If you have not already done so, have the applicant sign in to VA.gov
+          using their Login.gov account. 
+        </p>
+        <p>You may close this application window now.</p>
       </div>
     </div>
   );
