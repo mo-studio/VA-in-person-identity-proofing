13ce044af7d2bdf4ee16ecd9e3f0c1f729f9215b
Mateo Salinas Clarke <mateosalinasclarke@proton.me>
2023-11-27 23:23:20 -0700

    🤷‍♂️

--- app/src/pages/intake/confirm-site/index.jsx
+++ app/src/pages/intake/confirm-site/index.jsx
@@ -7,7 +7,8 @@ import { IntakeContext } from "../../../contexts/IntakeContext";
 export default function LocationConfirmationScreen() {
   const { location } = useContext(IntakeContext);
   const { name } = location.attributes;
-  const { address1, city, state, zip } = location.attributes.address.physical;
+  const { physical } = location.attributes.address;
+  const { address1, city, state, zip } = physical && physical;
 
   return (
     <div className="page">
