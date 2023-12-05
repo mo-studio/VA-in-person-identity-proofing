0eb1ef0f27c329cab0590bc65e0b8cec0e85bce1
Mateo Salinas Clarke <mateosalinasclarke@proton.me>
2023-12-05 01:48:38 -0700

    add more tests

--- app/tests/pages/proofing/1b-confirm-email.test.tsx
+++ app/tests/pages/proofing/1b-confirm-email.test.tsx
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react";
+import { ProofingContext } from "src/contexts/ProofingContext";
+import { initialProofingData } from "src/data/proofingData";
 import ConfirmEmailPage from "src/pages/proofing/1b-confirm-email";
 
 describe("ConfirmEmailPage", () => {
@@ -7,4 +9,30 @@ describe("ConfirmEmailPage", () => {
     const continueButton = screen.getByText("Continue");
     expect(continueButton).toBeEnabled();
   });
+
+  // it should display the case number from the previous page
+  it("should display the case number from the previous page", () => {
+    const defaultCaseNumber = "123456789";
+    const updatedData = {
+      ...initialProofingData,
+      caseNumber: defaultCaseNumber,
+    };
+
+    const setProofingData = (data: typeof initialProofingData) => {
+      return data;
+    };
+
+    render(
+      <ProofingContext.Provider
+        value={{ proofingData: updatedData, setProofingData }}
+      >
+        <ConfirmEmailPage />
+      </ProofingContext.Provider>
+    );
+    const caseNumber = screen.getByText(`Case Number: ${defaultCaseNumber}`);
+    expect(caseNumber).toBeInTheDocument();
+  });
+
+  // it should display the email address associated with the case number from the Login.gov payload
+  // TODO
 });
